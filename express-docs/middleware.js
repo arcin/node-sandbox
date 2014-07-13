cfb87515c86b02312a7d7f01d2fb758c9d412ba0
Charles Arciniega <charlesarcin@gmail.com>
2014-07-12 17:14:04 -0700

    playing with middleware execution chain

---
+++ express-docs/middleware.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next){
+  console.log("http: %s %s", req.method, req.url);
+  next();
+});
+
+app.use(function(req, res, next){
+  next();
+});
+
+app.get('/', function(req, res){
+  res.send('Hello Charles');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('listening on port ' + app.get('port'));
+});
