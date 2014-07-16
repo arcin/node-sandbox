67ab3ff948d83502797e83ed5ff1236a91a58bec
Charles Arciniega <charlesarcin@gmail.com>
2014-07-16 11:16:00 -0700

    log the net.Socket object associated with the connection

--- node-stdlib/http.js
+++ node-stdlib/http.js
@@ -1,5 +1,4 @@
 var http = require('http'),
-    url = require('url')
     port = process.env.PORT || 3000,
     message = 'success';
 
@@ -10,7 +9,9 @@ var server = http.createServer(function(req, res){
   console.log('request trailers: ', req.trailers);
   console.log('request method: ', req.method);
   console.log('request url: ', req.url);
-  console.log('parsed url: ', url.parse(req.url, true));
+  console.log('request socket: ', req.socket);
+
+
 
   res.writeHead(200, {
     'Content-Length': message.length,
