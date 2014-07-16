604b672c21c65b55ddf03b08c2fd58f598e8e9be
Charles Arciniega <charlesarcin@gmail.com>
2014-07-16 11:45:42 -0700

    make request

--- node-stdlib/request.js
+++ node-stdlib/request.js
@@ -5,3 +5,12 @@ var options = {
   port: 3000,
   method: 'GET',
 };
+
+var req = http.request(options, function(res){
+  res.setEncoding('utf-8');
+  res.on('data', function(data){
+    console.log('Response Body: ', data);
+  });
+});
+
+req.end();
