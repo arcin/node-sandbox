b1e3e54b43559a29c5fcfd1bdb96872c56398085
Charles Arciniega <charlesarcin@gmail.com>
2014-07-17 02:55:00 -0700

    test the 'finish' event. It's fired when the response has finished handing over the headers and body to the OS.

--- node-stdlib/http.js
+++ node-stdlib/http.js
@@ -4,19 +4,16 @@ var http = require('http'),
 
 
 var server = http.createServer(function(req, res){
-  console.log('request http version: ', req.httpVersion);
-  console.log('request headers: ', req.headers);
-  console.log('request trailers: ', req.trailers);
-  console.log('request method: ', req.method);
-  console.log('request url: ', req.url);
-//  console.log('request socket: ', req.socket);
 
-
-
+  res.on('finish', function(){
+    console.log('the response headers/body have been handed to the operating system');
+  });
   res.writeHead(200, {
     'Content-Length': message.length,
     'Content-Type': 'text/plain'
   });
+
+  console.log(new Date().getTime());
   res.end(message);
 });
 
