90cc6f9e2ffd529422ecfff52f7b4ac42a07dd5c
Charles Arciniega <charlesarcin@gmail.com>
2014-07-18 14:40:12 -0700

    do some basic document insertion with mongo-native driver

--- node-stdlib/http.js
+++ node-stdlib/http.js
@@ -4,11 +4,18 @@ var http = require('http'),
     message = 'success';
 
 MongoClient.connect("mongodb://localhost:27017/exampleDB", function(err, db){
-  if (!err) {
-    console.log('connection successful');
-    db.createCollection('test', function(err, collection){});
-  }
   if (err) console.log(err.message);
+
+  console.log('connection successful');
+
+  var collection = db.collection('test');
+  var doc1 = { hello: 'doc1' };
+  var doc2 = { hello: 'doc2' };
+  var lotsOfDocs = [ { hello:'doc3' }, { hello:'doc4' } ];
+
+  collection.insert(doc1, function(err, result){});
+  collection.insert(doc2, {w:1}, function(err, result){});
+  collection.insert(lotsOfDocs, {w:1}, function(err, result){});
 });
 
 
