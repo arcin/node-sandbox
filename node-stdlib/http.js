c6afa8b2a7b46345761965e50cd73698dbd7bb2f
Charles Arciniega <charlesarcin@gmail.com>
2014-07-18 15:03:27 -0700

    insert and update documents using mongo operations

--- node-stdlib/http.js
+++ node-stdlib/http.js
@@ -7,15 +7,19 @@ MongoClient.connect("mongodb://localhost:27017/exampleDB", function(err, db){
   if (err) console.log(err.message);
 
   console.log('connection successful');
-
   var collection = db.collection('test');
-  var doc1 = { hello: 'doc1' };
-  var doc2 = { hello: 'doc2' };
-  var lotsOfDocs = [ { hello:'doc3' }, { hello:'doc4' } ];
+  var doc = { mykey:1, fieldtoupdate:1 };
 
-  collection.insert(doc1, function(err, result){});
-  collection.insert(doc2, {w:1}, function(err, result){});
-  collection.insert(lotsOfDocs, {w:1}, function(err, result){});
+  collection.insert(doc, {w:1}, function(err, result){
+    collection.update({ mykey:1 }, { $set:{ fieldtoupdate:2 } }, { w:1 }, function(err, result) {});
+    collection.update({ hello: "doc4" }, { $set:{ hello: 'special doc' } }, {w:1}, function(err, result){});
+  });
+
+  var doc2 = { mykey:2, docs:[ { doc1:1 } ] };
+
+  collection.insert(doc2, { w:1 }, function(err, result){
+    collection.update( { mykey:2 }, { $push:{ docs:{ doc2:1 } } },  {w:1}, function(err, result){});
+  });
 });
 
 
