1c73156a0e5b4a5bb6d19645775e84c86e3fe512
Charles Arciniega <charlesarcin@gmail.com>
2014-07-18 00:35:03 -0700

    make a connection to mongo and create a test collection

--- node-stdlib/http.js
+++ node-stdlib/http.js
@@ -1,10 +1,18 @@
 var http = require('http'),
     port = process.env.PORT || 3000,
+    MongoClient = require('mongodb').MongoClient,
     message = 'success';
+
+MongoClient.connect("mongodb://localhost:27017/exampleDB", function(err, db){
+  if (!err) {
+    console.log('connection successful');
+    db.createCollection('test', function(err, collection){});
+  }
+  if (err) console.log(err.message);
+});
 
 
 var server = http.createServer(function(req, res){
-
   res.on('finish', function(){
     console.log('the response headers/body have been handed to the operating system');
   });
